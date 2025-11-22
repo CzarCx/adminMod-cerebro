633e410f3c3273e3ad86b65a404091f7c9427f67
jj fz <fzjj3836@gmail.com>
2025-11-22 16:13:52 +0000

    elimina el boton de escanear

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -42,12 +42,6 @@ export default function Navbar() {
               <NavLink href="/registros-historicos">Historial</NavLink>
               <NavLink href="/seguimiento-de-etiquetas">Etiquetas</NavLink>
               <NavLink href="/reportes">Reportes</NavLink>
-              <Link
-                href="/scanner"
-                className="inline-flex h-9 items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-              >
-                Escanear
-              </Link>
             </div>
           </div>
         </div>
